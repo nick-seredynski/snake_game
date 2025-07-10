20a739ae8e9fad1ff22eafe60d09d9bee821e463
Nick <87275718+nick-seredynski@users.noreply.github.com>
2025-07-10 04:49:15 +0100

    Update snake_dependency.py

--- snake_dependency.py
+++ snake_dependency.py
@@ -1,8 +1,10 @@
 
 from turtle import Turtle
 start_pos = [(0, 0), (-20, 0), (-40, 0)]
+move_distance = 20
 
 class Snake:
+
     def __init__(self):
         self.segments = []
         self.create_segments()
@@ -10,9 +12,17 @@ class Snake:
 
     def create_segments(self):
         for positions in start_pos:
-            self.segments = Turtle("square")
-            self.segments.shape("square")
-            self.segments.penup()
-            self.segments.color("white")
-            self.segments.goto(positions)
+            new_seg = Turtle("square")
+            new_seg.shape("square")
+            new_seg.penup()
+            new_seg.color("white")
+            new_seg.goto(positions)
+            self.segments.append(new_seg)
 
+
+    def move_segments(self):
+        for seg in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg - 1].xcor()
+            new_y = self.segments[seg - 1].ycor()
+            self.segments[seg].goto(new_x, new_y)
+        self.segments[0].forward(move_distance)
