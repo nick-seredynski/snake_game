e01a1a3f0050e957abb5bd55baf8137870e7491f
Nick <87275718+nick-seredynski@users.noreply.github.com>
2025-07-10 15:10:15 +0100

    Update test_movement.py
    Movement test in independent module

--- test_movement.py
+++ test_movement.py
@@ -1,22 +1,24 @@
 
+
+
 from turtle import Turtle, Screen
-import time
+
 
 screen = Screen()
 tim = Turtle()
 
 def move_left():
-    tim.setheading(0)
-    tim.forward(10)
-
-def move_right():
     tim.setheading(180)
     tim.forward(10)
 
-start = True
-while start:
+def move_right():
+    tim.setheading(0)
+    tim.forward(10)
 
-    screen.onkey(move_left, key="Left")
-    screen.onkey(move_right, key="Right")
 
-    screen.listen()
+screen.onkey(move_left, key="Left")
+screen.onkey(move_right, key="Right")
+
+screen.listen()
+
+screen.exitonclick()
