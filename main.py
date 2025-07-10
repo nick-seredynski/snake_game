1b91bd9c70d811ce743a12fadf774c8802706e2c
Nick <87275718+nick-seredynski@users.noreply.github.com>
2025-07-10 04:48:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,29 +2,21 @@
 import time
 from turtle import Turtle, Screen
 from snake_dependency import Snake
-turtle_list = []
 
-tim = Turtle()
 screen = Screen()
-x_pos = 0
 
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake game")
 screen.tracer(0)
 
-Snake()
 
-screen.update()
+snake = Snake()
+
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.5)
 
-    for seg in range(len(turtle_list) - 1, 0, -1):
-        new_x = turtle_list[seg - 1].xcor()
-        new_y = turtle_list[seg - 1].ycor()
-        turtle_list[seg].goto(new_x, new_y)
-    turtle_list[0].forward(20)
-
+    snake.move_segments()
