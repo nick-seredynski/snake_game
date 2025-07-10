ae0ee0630c8fbcbe1c9b5ea6403dfc433fe55c0b
Nick <87275718+nick-seredynski@users.noreply.github.com>
2025-07-10 23:42:52 +0100

    Update snake_dependency.py
    movement enabled

--- snake_dependency.py
+++ snake_dependency.py
@@ -9,7 +9,6 @@ class Snake:
         self.segments = []
         self.create_segments()
 
-
     def create_segments(self):
         for positions in start_pos:
             new_seg = Turtle("square")
@@ -19,10 +18,26 @@ class Snake:
             new_seg.goto(positions)
             self.segments.append(new_seg)
 
-
     def move_segments(self):
         for seg in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg - 1].xcor()
             new_y = self.segments[seg - 1].ycor()
             self.segments[seg].goto(new_x, new_y)
         self.segments[0].forward(move_distance)
+
+    def turn_left(self):
+        if self.segments[0].heading() != 0:
+            self.segments[0].setheading(180)
+
+    def turn_right(self):
+        if self.segments[0].heading() != 180:
+            self.segments[0].setheading(0)
+
+    def turn_up(self):
+        if self.segments[0].heading() != 270:
+            self.segments[0].setheading(90)
+
+    def turn_down(self):
+        if self.segments[0].heading() != 90:
+            self.segments[0].setheading(270)
+
