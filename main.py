7b5a16499a95784ba25377b7a7976c54b7f2fb27
Nick <87275718+nick-seredynski@users.noreply.github.com>
2025-07-13 10:10:19 +0100

    Update main.py
    Added food object to game

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 
 import time
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake_dependency import Snake
+from food import Food
 
 screen = Screen()
 
@@ -10,13 +11,22 @@ screen.bgcolor("black")
 screen.title("Snake game")
 screen.tracer(0)
 
-
 snake = Snake()
+food = Food()
 
+screen.onkey(snake.turn_left, key="Left")
+screen.onkey(snake.turn_right, key="Right")
+screen.onkey(snake.turn_up, key="Up")
+screen.onkey(snake.turn_down, key="Down")
+
+screen.listen()
 
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.5)
-
+    time.sleep(0.2)
     snake.move_segments()
+
+    if snake.segments[0].distance(food) < 15:
+        food.new_food()
+
