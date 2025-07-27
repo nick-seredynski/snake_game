323971f27d96554ec361620d20d7a8f3ff2fdeb0
Nick <87275718+nick-seredynski@users.noreply.github.com>
2025-07-27 23:11:23 +0100

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,29 +1,37 @@
 
 from turtle import Turtle
 
-
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.goto(0, 260)
         self.score = 0
+        with open("data.txt", mode="r") as file:
+            self.high_score = file.read()
         self.penup()
         self.speed("fastest")
         self.color("white")
         self.hideturtle()
-        self.write(f"Score: {self.score}", True, "center", ('arial', 24, 'normal'))
+        self.write(f"Score: {self.score} High Score: {self.high_score}", True, "center", ('arial', 24, 'normal'))
 
 
     def update_score(self):
         self.clear()
         self.goto(0, 260)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", True, "center", ('arial', 24, 'normal'))
+
+    def increase_score(self):
         self.score += 1
-        self.write(f"Score: {self.score}", True, "center", ('arial', 24, 'normal'))
+        self.update_score()
+
+    def reset(self):
+        if self.score > int(self.high_score):
+            self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.high_score))
+        self.score = 0
+        self.update_score()
 
 
-    def game_over(self):
-        self.clear()
-        self.goto(0, 0)
-        self.write(f"GAME OVER. FINAL SCORE: {self.score}", True, "center", ('arial', 24, 'normal'))
 
 
